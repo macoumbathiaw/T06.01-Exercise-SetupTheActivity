4fa8f1d672eb6e2694d6039972fb1bf9e7cf786a
Macoumba Thiaw <macoumbathiaw00@gmail.com>
2018-03-28 06:59:06 -0700

    Initial commit

---
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,36 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //add up button on activity
+        ActionBar actionBar = this.getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+    }
+    //make sure the home button doesn't close the app
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
